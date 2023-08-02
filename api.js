e6405d23e8f431909ef46e58b4d257b94870bedc
PhilisterD <junboforwork@icloud.com>
2023-08-02 18:28:40 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,30 +1,43 @@
-// api.js
+// server.js
 
-const BASE_URL = 'https://api.openai.com/v1/chat/completions';  // ChatGPT API URL
-const API_KEY = process.env.OPENAI_API_KEY;  // Access the environment variable
+const express = require('express');
+const axios = require('axios');
 
-async function callChatGPT(prompt) {
+const app = express();
+const port = process.env.PORT || 3000;  // Set your desired port number
+
+// Middleware to parse JSON in request body
+app.use(express.json());
+
+// ChatGPT API endpoint
+const BASE_URL = 'https://api.openai.com/v1/chat/completions';
+const API_KEY = process.env.OPENAI_API_KEY;
+
+app.post('/api/chat', async (req, res) => {
+  const prompt = req.body.prompt;
+
   const data = {
     model: 'gpt-3.5-turbo',
     messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: prompt }]
   };
 
   try {
-    const response = await fetch(BASE_URL, {
-      method: 'POST',
+    const response = await axios.post(BASE_URL, data, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`  // Use the environment variable
-      },
-      body: JSON.stringify(data)
+        'Authorization': `Bearer ${API_KEY}`
+      }
     });
 
-    const responseData = await response.json();
-    return responseData.choices[0].message.content;
+    const responseData = response.data;
+    const content = responseData.choices[0].message.content;
+    res.json({ response: content });
   } catch (error) {
     console.error('Error calling ChatGPT API:', error);
-    return 'An error occurred while processing your request.';
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
   }
-}
+});
 
-export { callChatGPT };
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
