789499bdca9ee644c262e8a6591e0978c9aaf167
PhilisterD <junboforwork@icloud.com>
2023-10-26 19:28:03 +0800

    sync from mac

--- convert.py
+++ convert.py
@@ -1,24 +1,25 @@
 from PIL import Image
 import os
 
-# 指定包含JPG文件的文件夹
-input_folder = "images"
+# 设置文件夹路径
+data_folder = "images"
 
-# 创建一个目标文件夹，用于保存转换后的WEBP文件
-output_folder = "output"
-os.makedirs(output_folder, exist_ok=True)
+# 获取data文件夹下的所有文件
+file_list = os.listdir(data_folder)
 
-# 列出输入文件夹中的所有JPG文件
-jpg_files = [f for f in os.listdir(input_folder) if f.lower().endswith('.jpg')]
+# 循环处理每个文件
+for file_name in file_list:
+    if file_name.endswith(".jpg"):
+        # 构建jpg文件的完整路径
+        jpg_path = os.path.join(data_folder, file_name)
 
-# 遍历JPG文件并将其转换为WEBP格式
-for jpg_file in jpg_files:
-    jpg_path = os.path.join(input_folder, jpg_file)
-    webp_file = os.path.splitext(jpg_file)[0] + '.webp'  # 新的WEBP文件名
-    webp_path = os.path.join(output_folder, webp_file)
+        # 构建png文件的完整路径
+        png_path = os.path.join(data_folder, os.path.splitext(file_name)[0] + ".png")
 
-    # 打开JPG文件并保存为WEBP
-    img = Image.open(jpg_path)
-    img.save(webp_path, 'webp')
+        # 打开jpg文件
+        img = Image.open(jpg_path)
 
-print("转换完成。")
+        # 保存为png文件
+        img.save(png_path, "PNG", quality=60)
+
+print("转换完成")
