df73787c86efefb9171b80435c4bb95f75325394
PhilisterD <junboforwork@icloud.com>
2023-11-02 11:16:16 +0800

    sync from mac

--- script.js
+++ script.js
@@ -1,29 +1,108 @@
-const password_ele = document.getElementById("pwd_txt");
-var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZacdefghijklnopqrstuvwxyz0123456789";
-const special_chars = "@#$%^&*";
-const generate = document.getElementById("generate");
-const clipboard = document.getElementById("clipboard");
-var pwd_length = document.getElementById("slider");
+const APIURL = "https://api.themoviedb.org/3/discover/movie?language=zh&api_key=04c35731a5ee918f014970082a0088b1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const SEARCHIDAPI = "https://api.themoviedb.org/3/movie/{id}?language=zh-CN&api_key=04c35731a5ee918f014970082a0088b1"
 
 
-generate.addEventListener('click', () => {
-    let password = "";
-    var checked = document.getElementById("checkbox").checked;
-    var final_string = string;
-    console.log(checked);
-    if (checked) {
-        final_string += "@#$%^&*";
+const main = document.getElementById("content");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const fileName = 'data.json';
+// initially get fav movies
+
+const ids = [575264, 912908, 298618, 252822, 882569, 335977, 763165, 1018332, 856289, 1112175]
+// showMovies(data)
+getMoviesList();
+
+async function replaceId(ids) {
+  const newUrls = ids.map((id) => {
+    return SEARCHIDAPI.replace("{id}", id);
+  });
+  return newUrls;
+}
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    console.log(respData);
+    showMovies(respData.results);
+}
+
+async function getMoviesList() {
+    const respData = [];
+    const urllist = await replaceId(ids);
+    for (const url of urllist) {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        respData.push(data);
     }
-    for (var i = 0; i < pwd_length.value; i++) {
-        let pwd = final_string[Math.floor(Math.random() * final_string.length)];
-        password += pwd;
+    console.log(respData);
+    showMovies(respData);
+}
+
+function getJson() {
+    $.ajax({
+        type: "GET",
+        url: "config.json",
+        'async':false,
+        dataType: "json",
+        success: (res) => {
+            getdata = res
+        }
+    })
+}
+
+function showMovies(movies) {
+    
+    // clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
     }
-    password_ele.innerText = password;
-    final_string = string;
-});
+}
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-clipboard.addEventListener('click', () => {
-    navigator.clipboard.writeText(password_ele.innerText);
-    alert("Password copied to clipboard");
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
 });
